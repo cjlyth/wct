d505e2f2893a56e74fde02825762ecdbbb5e0f66
Christopher J Lyth <cjlyth@gmail.com>
2014-05-23 10:29:12 -0400

    forgot to add RUN

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./build.sh mysql-provided && ./build.sh tomcat-provided && ./build.sh wct-provided && ./build.sh wct
+./build.sh mysql-provided && ./build.sh tomcat-provided && ./build.sh wct-provided && ./build.sh wct && ./run.sh wct "${@:-supervisord -e debug -k}"
