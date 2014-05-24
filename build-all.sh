94ac3164d4a67557dedd0fb5c0843aca0e336cfb
Christopher Lyth <lyth_christopher@bah.com>
2014-05-24 03:58:18 -0400

    worked on the structure a little

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-./build.sh mysql-provided && ./build.sh tomcat-provided && ./build.sh wct-provided && ./build.sh wct && ./run.sh wct "${@:-supervisord -e debug -k}"
+./build.sh mysql-provided && ./build.sh tomcat-provided && ./build.sh wct-provided && ./build.sh wct 
+./run.sh wct "${@:-"supervisord -e debug -k"}"
